f9faad9fa8380cc551ff5dea474820216ab594db
beautyheheer <135201166+beautyheheer@users.noreply.github.com>
2023-11-22 20:20:20 +0800

    Update map_visualization.r

--- map_visualization.r
+++ map_visualization.r
@@ -1,25 +1,26 @@
-#【是一段啥也没做出来的代码 哈哈哈！下次再改】
-# 导入所需的包
+#【地图！！】
+install.packages("maps")
+
 library(ggplot2)
-library(dplyr)
+library(maps)
 
-# 读取数据文件
-data <- read.csv("C:/Users/LENOVO/Desktop/lab/Filtered_data_code_PEP/st BBCH 13 leaf 0.5.csv", header = TRUE)
+# 读取CSV文件
+data <- read.csv("C:/Users/LENOVO/Desktop/lab/Filtered_data_code_PEP/DE_11.csv")
 
-# 计算每个 PEP_ID 的数据量
-data_count <- data %>%
-  group_by(PEP_ID) %>%
-  summarize(count = n())
+world = map_data("world")
 
-# 合并数据
-data_merged <- merge(data, data_count, by = "PEP_ID")
+world = subset(world, lat > 47.3 & lat < 55.2 & long > 5.5 & long < 15.2)
 
-# 获取地图
-map <- get_googlemap(center = c(lon = mean(data_merged$LON), lat = mean(data_merged$LAT)), zoom = 10, scale = 2)
+data_pos = data[,c("LON","LAT")]
+data_pos = unique(data_pos)
+data_pos = round(data_pos,digits = 1)
+data_pos = unique(data_pos)
 
-# 绘制地图
-ggmap(map) +
-  geom_point(data = data_merged, aes(x = LON, y = LAT, size = count, color = "red")) +
-  scale_size_continuous(range = c(3, 10)) +
-  labs(title = "PEP_ID Data Map") +
-  theme_minimal()
+
+ggplot() +
+  geom_map(data = world, map = world,
+           aes(long, lat, map_id = region),
+           color = "white", fill = "grey", size = 0.1) +
+  
+  geom_point(data = data_pos,
+             aes(x = LON, y = LAT ),shape=21)
